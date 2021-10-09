dcc8c30d39649ecfa22d67dc7a81c7c94751a268
jaehyun <wogus3132@naver.com>
2021-10-09 10:51:55 +0900

    어뎁터 뼈대 작업 마무리

--- app/src/main/java/com/example/listview_jaehyun/adapters/studentAdapter.kt
+++ app/src/main/java/com/example/listview_jaehyun/adapters/studentAdapter.kt
@@ -1,11 +1,30 @@
 package com.example.listview_jaehyun.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listview_jaehyun.R
 import com.example.listview_jaehyun.dates.Student
 
 class StudentAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId , mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+
+        }
+        val row = tempRow!!
+
+        return  row
+
+    }
 }
