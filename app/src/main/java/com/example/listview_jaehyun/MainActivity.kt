8184d2193006eb96fd8488c951b7fabc682f1dc7
jaehyun <wogus3132@naver.com>
2021-10-09 13:07:19 +0900

    학생목록을 담 을 ArrayList 생성

--- app/src/main/java/com/example/listview_jaehyun/MainActivity.kt
+++ app/src/main/java/com/example/listview_jaehyun/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.listview_jaehyun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listview_jaehyun.dates.Student
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add( Student("이재현",1997) )
+        mStudentList.add( Student("이창근",1996) )
+        mStudentList.add( Student("윤동근",1998) )
+        mStudentList.add( Student("이상렬",1995) )
+        mStudentList.add( Student("조성범",1995) )
+        mStudentList.add( Student("박동규",1996) )
+        mStudentList.add( Student("이하이",1996) )
     }
 }
